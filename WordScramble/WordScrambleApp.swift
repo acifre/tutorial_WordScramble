d2b9c2dca9391b57b3ced0af5ee094c3234501e4
acifre <78503928+acifre@users.noreply.github.com>
2023-01-29 16:41:51 -0600

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Anthony Cifre on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
